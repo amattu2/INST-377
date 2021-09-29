4846619150c11773d8b38a6d7ec00d86b2ff6633
Alec M <alecmattu1234@gmail.com>
2021-09-29 08:41:54 -0400

    Implement createDoodler

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -4,3 +4,30 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const doodler = createDoodler(grid);
 
 });
+
+/**
+ * Create a new doodler for the game
+ *
+ * @param {DOMElement} grid
+ * @return {DOMElement} new doodler
+ * @author Alec M. <https://amattu.com>
+ * @date 2021-09-29T08:33:46-040
+ */
+function createDoodler(grid) {
+  // Checks
+  if (!grid) return null;
+
+  // Element
+  const div = document.createElement('div');
+
+  // Attributes
+  div.classList.add('doodler');
+  div.style.left = "50px";
+  div.style.bottom = "150px";
+
+  // Insert element
+  grid.appendChild(div);
+
+  // Return handle
+  return div;
+}
